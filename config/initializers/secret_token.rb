eb8def059c411860f477530989a52006fa8f3914
Graham Hadgraft <graham.hadgraft@gmail.com>
2013-01-03 11:52:20 +0000

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlidecastCreator::Application.config.secret_token = '17a7872ef3c5b8ce122f77538e3902d2a79bcc8fdc43cb3419f1a155e53ac2edde7f6e8ba1ec8c4c6548de983f55b32ad5fa7f966ebc87e921699257e4d1f0d5'
