6f606b6ab00f3bc8f2c48a811a2ef304902e00c9
Graham Hadgraft <graham.hadgraft@gmail.com>
2013-01-03 16:21:51 +0000

    added conversion of slides from pdf to images

--- app/controllers/slidecast_controller.rb
+++ app/controllers/slidecast_controller.rb
@@ -1,5 +1,11 @@
 class SlidecastController < ApplicationController
   def index
   	@video = 'confex.mp4'
+  	@slides = 'away-day.pdf'
+
+  	#mkdir to ouput files to
+  	@output = `docsplit images #{Rails.root}/public/slides/#{@slides} --size 700x --format png --output #{Rails.root}/public/slides/#{@slides}-images/`
+  	#Docsplit.extract_images("#{RailsRoot}/public/slides/#{@slides}", :size => '920x240', :format => [:png], :output => "#{RailsRoot}/public/slides/#{@slides}/")
+  
   end
 end
