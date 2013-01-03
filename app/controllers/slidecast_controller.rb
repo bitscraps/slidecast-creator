daee194159d4f45ad8466594b35c8cbf31f198c2
Graham Hadgraft <graham.hadgraft@gmail.com>
2013-01-03 14:43:05 +0000

    added a javascript video timeline with updated thumbnail

---
+++ app/controllers/slidecast_controller.rb
@@ -0,0 +1,5 @@
+class SlidecastController < ApplicationController
+  def index
+  	@video = 'confex.mp4'
+  end
+end
---
+++ test/unit/helpers/slidecast_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class SlidecastHelperTest < ActionView::TestCase
+end
